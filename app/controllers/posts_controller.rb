c8fb3a01fd718c47f3633705af8b2878e2b042a6
Pavel Konkin <idmfani@gmail.com>
2023-05-02 17:59:47 +0300

    Place authenticate_user! first in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!
+  before_action :set_post, only: %i[show edit update destroy]
   # GET /posts/1 or /posts/1.json
   def show
     @post_comment = @post.comments.build
